5fe5e3df7cb632a9771b3dcc0bc570b3818e640c
fridev <96908551+magrigsdev@users.noreply.github.com>
2023-08-18 11:18:01 +0200

    creation des tables

--- database/migrations/2023_08_18_081653_init_mzrenovation.php
+++ database/migrations/2023_08_18_081653_init_mzrenovation.php
@@ -13,7 +13,90 @@ class InitMzrenovation extends Migration
      */
     public function up()
     {
-        //
+        //creation des tables 
+
+        //devis
+        Schema::create('devis', function(Blueprint $table){
+            $table->id();
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->string('titre');
+            $table->string('description');
+            $table->integer('montants');
+            $table->string('valider');
+
+            //relation devis et clients
+            $table->unsignedBigInteger('clients_id');
+            $table->foreign('clients_id')->references('id')->on('clients');
+        });
+
+        //travaux
+        Schema::create('travaux', function(Blueprint $table){
+            $table->id();
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->string('titre');
+            $table->string('description');
+            $table->integer('montants');
+
+            //relation travaux et clients
+            $table->unsignedBigInteger('clients_id');
+            $table->foreign('clients_id')->references('id')->on('clients');
+        });
+
+        //clients
+        Schema::create('clients', function(Blueprint $table){
+            $table->id();
+            $table->string('nom');
+            $table->string('email');
+            $table->string('sexe');
+            $table->integer('tel');
+            $table->integer('adresse');
+           
+
+        });
+        //profil
+        Schema::create('profil', function(Blueprint $table){
+            $table->id();
+            $table->string('nom');
+            $table->string('email');
+            $table->string('sexe');
+            $table->string('is_admin');
+            $table->integer('tel');
+        });
+
+        //utilisateurs
+        Schema::create('utilisateurs', function(Blueprint $table){
+            $table->id();
+            $table->string('nom');
+            $table->string('email');
+            $table->string('motpasse');
+
+            //relation utilsateurs et profil
+            $table->unsignedBigInteger('profil_id');
+            $table->foreign('profil_id')->references('id')->on('profil');           
+        });
+
+        //admin
+        Schema::create('admin', function(Blueprint $table){
+            $table->id();
+            $table->string('nom');
+            $table->string('email');
+            $table->string('motpasse');
+
+            //relation admin et profil
+            $table->unsignedBigInteger('profil_id');
+            $table->foreign('profil_id')->references('id')->on('profil');           
+        });
+
+        //commentaires
+        Schema::create('commentaires', function(Blueprint $table){
+            $table->id();
+            $table->string('nom');
+            $table->string('email');
+            $table->string('message');           
+        });
+
     }
 
     /**
@@ -24,5 +107,12 @@ class InitMzrenovation extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('clients');
+        Schema::dropIfExists('profil');
+        Schema::dropIfExists('travaux');
+        Schema::dropIfExists('utilsateurs');
+        Schema::dropIfExists('admin');
+        Schema::dropIfExists('commentaires');
+        Schema::dropIfExists('devis');
     }
 }
